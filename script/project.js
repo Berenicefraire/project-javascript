83dfe997ff0fe269e14b3076f75ee38792e8b78a
Bere Fraire Heredia <berenicefraireh@outlook.com>
2022-09-01 11:41:14 -0500

    creat a new table with the input information

--- script/project.js
+++ script/project.js
@@ -1,14 +1,12 @@
 function myFunction(){
     let nombreElement = document.getElementById("name");
     let nombreValue = nombreElement.value;
-    let res = '';
     if(nombreValue.length !== 0 && nombreValue !== " ") {
         console.log("cargando...");
         fetchData(nombreValue)
         .then(response => response.json())
         .then(data => {
-            res = data;
-            console.log(res);
+            makeTable(data);
             console.log("termine...");
         });
     } else {
@@ -16,8 +14,34 @@ function myFunction(){
     }
     return false;
 }
-
+//fetchData ahora puede tomar propiedades de una promesa
 function fetchData(url){
     //fetchData regresa una promesa
     return fetch(url);
 } 
+
+function makeTable(objData){
+    let tableRef = document.getElementsByTagName("tbody")[0];
+
+    for(obj of objData) {
+        
+        //para crear el cuerpo de la tabla
+        let fila = document.createElement("tr");
+        
+        //crea nodos te texto con el contenido de cada celda de la tabla
+        let td = document.createElement('td');
+
+        td.innerText = obj.id;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = obj.name;
+        fila.appendChild(td);
+
+        td = document.createElement("td");
+        td.innerText = obj.username;
+        fila.appendChild(td);
+
+        tableRef.appendChild(fila);
+    }
+}
